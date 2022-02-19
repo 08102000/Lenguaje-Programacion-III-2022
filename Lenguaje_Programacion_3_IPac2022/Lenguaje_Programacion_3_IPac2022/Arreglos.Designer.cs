b64b91c8c34cc74593037dbd8a4ae651aa3dd3bb
08102000 <miltonamador1508@gmail.com>
2022-02-18 21:26:53 -0600

    Ejercicio arreglos
    Ejecuta los nombres y edades ingresados por el usuario y se almacenan en un arraylist y se muestran.

---
+++ Lenguaje_Programacion_3_IPac2022/Lenguaje_Programacion_3_IPac2022/Arreglos.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Lenguaje_Programacion_3_IPac2022
+{
+    partial class Arreglos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.EdadtextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Agregarbutton = new System.Windows.Forms.Button();
+            this.Mostrarbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(216, 36);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(100, 20);
+            this.NombretextBox.TabIndex = 2;
+            // 
+            // EdadtextBox
+            // 
+            this.EdadtextBox.Location = new System.Drawing.Point(216, 125);
+            this.EdadtextBox.Name = "EdadtextBox";
+            this.EdadtextBox.Size = new System.Drawing.Size(100, 20);
+            this.EdadtextBox.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(144, 261);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(265, 121);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ingrese el Nombre ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ingrese la Edad";
+            // 
+            // Agregarbutton
+            // 
+            this.Agregarbutton.Location = new System.Drawing.Point(144, 187);
+            this.Agregarbutton.Name = "Agregarbutton";
+            this.Agregarbutton.Size = new System.Drawing.Size(105, 23);
+            this.Agregarbutton.TabIndex = 9;
+            this.Agregarbutton.Text = "Agregar Datos";
+            this.Agregarbutton.UseVisualStyleBackColor = true;
+            this.Agregarbutton.Click += new System.EventHandler(this.Agregarbutton_Click);
+            // 
+            // Mostrarbutton
+            // 
+            this.Mostrarbutton.Location = new System.Drawing.Point(304, 187);
+            this.Mostrarbutton.Name = "Mostrarbutton";
+            this.Mostrarbutton.Size = new System.Drawing.Size(105, 23);
+            this.Mostrarbutton.TabIndex = 10;
+            this.Mostrarbutton.Text = "Mostrar Datos";
+            this.Mostrarbutton.UseVisualStyleBackColor = true;
+            this.Mostrarbutton.Click += new System.EventHandler(this.Mostrarbutton_Click);
+            // 
+            // Arreglos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Mostrarbutton);
+            this.Controls.Add(this.Agregarbutton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.EdadtextBox);
+            this.Controls.Add(this.NombretextBox);
+            this.Name = "Arreglos";
+            this.Text = "Arreglos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.TextBox EdadtextBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Agregarbutton;
+        private System.Windows.Forms.Button Mostrarbutton;
+    }
+}
---
+++ Lenguaje_Programacion_3_IPac2022/Lenguaje_Programacion_3_IPac2022/Arreglos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lenguaje_Programacion_3_IPac2022
+{
+    public partial class Arreglos : Form
+    {
+        ArrayList NombreAlumnos;
+        ArrayList EdadAlumnos;
+        public Arreglos()
+        {
+            InitializeComponent();
+            NombreAlumnos = new ArrayList();
+            EdadAlumnos = new ArrayList();
+        }
+
+       
+        //Utilice un ArrayList por que tuve problemas al hacerlo con un arreglo sencillo, el programa funciona bien guarda los datos.
+      
+
+        private void Agregarbutton_Click(object sender, EventArgs e)
+        {
+            NombreAlumnos.Add(NombretextBox.Text);
+            EdadAlumnos.Add(Convert.ToInt32(EdadtextBox.Text));
+        }
+
+        private void Mostrarbutton_Click(object sender, EventArgs e)
+        {
+            for (int i=0; i<NombreAlumnos.Count; i++)
+            {
+                listBox1.Items.Add("El nombre del Alumno es: " + NombreAlumnos[i] + " Edad: " + EdadAlumnos[i] + " Años ");
+            }
+        }
+    }
+
+}
